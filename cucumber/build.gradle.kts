32738636e2693f763b5bf4d612b26f5001389e49
James <mejrbayliss@gmail.com>
2023-07-07 16:25:32 +0100

    Adds most simple cucumber test

--- cucumber/build.gradle.kts
+++ cucumber/build.gradle.kts
@@ -1,19 +1,28 @@
 plugins {
-    id("java")
+    java
 }
 
 group = "org.example"
 version = "unspecified"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testImplementation(platform("io.cucumber:cucumber-bom:7.13.0"))
+
+    testImplementation("io.cucumber:cucumber-java")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine")
+    testImplementation("org.junit.platform:junit-platform-suite")
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // Work around. Gradle does not include enough information to disambiguate
+    // between different examples and scenarios.
+    systemProperty("cucumber.junit-platform.naming-strategy", "long")
 }
